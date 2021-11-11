ccc28fa8e48de8fb2240bae6775e7bfb8587e5c1
Akshay Subramanian <akshaysubramanian99@gmail.com>
2021-11-12 02:03:19 +0530

    Rename sample_html3.html -> sample_html.html

--- test_pipeline_single.py
+++ test_pipeline_single.py
@@ -1,8 +1,7 @@
 from pipeline_single import run_pipeline_single
 
-f = open('sample_html3.html', 'r')
+f = open('sample_html.html', 'r')
 data = str(f.read())
-#print(data)
 f.close()
 
 run_pipeline_single('extracted_data_single', False, "rsc", data)
